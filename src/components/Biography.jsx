5a16de33f21c95e7f9d10245fd50c87c20ece83a
Osman Kahraman <iam@osmankahraman.eu>
2025-07-02 01:05:23 +0200

    Changed lorem ipsum in the Biography with correct text and changed from list element to custom div elements

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -2,15 +2,39 @@ function Biography() {
   return (
     <main>
       <h1>Biography</h1>
-      <h3>Osman Kahraman</h3>
+      <section className="main-list-section">
+        <h3>Professional</h3>
+        <div className="bio-list-div">
+          <div className="bio-list-item-div">
+            <p className="bio-list-date-p">09.2021 - 05.2022</p>
+            <p>Worker in food service</p>
+          </div>
+          <div className="bio-list-item-div">
+            <p className="bio-list-date-p">02.2025 - 07.2025</p>
+            <p>Junior Software Developer</p>
+          </div>
+        </div>
+      </section>
       <section className="main-list-section">
         <h3>Education</h3>
-        <ul>
-          <li>01/01 - 12/02 Thing 1</li>
-          <li>01/01 - 12/02 Thing 1</li>
-          <li>01/01 - 12/02 Thing 1</li>
-          <li>01/01 - 12/02 Thing 1</li>
-        </ul>
+        <div className="bio-list-div">
+          <div className="bio-list-item-div">
+            <p className="bio-list-date-p">09.2012 - 06.2018</p>
+            <p>Secondary School</p>
+          </div>
+          <div className="bio-list-item-div">
+            <p className="bio-list-date-p">08.2018 - 07.2021</p>
+            <p>High School</p>
+          </div>
+          <div className="bio-list-item-div">
+            <p className="bio-list-date-p">10.2022 - 05.2024</p>
+            <p>B.Sc. Physics (no degree acquired)</p>
+          </div>
+          <div className="bio-list-item-div">
+            <p className="bio-list-date-p">08.2025 - currently</p>
+            <p>Apprenticeship as Software Developer</p>
+          </div>
+        </div>
       </section>
     </main>
   );
