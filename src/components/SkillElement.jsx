b5afaf0dd7fca904478a66941dde468a9e31bb82
Osman Kahraman <iam@osmankahraman.eu>
2025-07-04 01:21:02 +0200

    Changed Work to load entries from JSON file instead of using static text and the styling

--- src/components/SkillElement.jsx
+++ src/components/SkillElement.jsx
@@ -1,8 +1,8 @@
 function SkillElement(props) {
   return (
     <div className="skill-item-div">
-        <img className="skill-icon" src={props.image}/>
-        <p className="skill-title-p">{props.title}</p>
+      <img className="skill-icon" src={"/src/assets/icons/" + props.icon} />
+      <p className="skill-title-p">{props.title}</p>
     </div>
   );
 }
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,32 +1,51 @@
+import { Fragment } from "react/jsx-runtime";
 import SkillElement from "/src/components/SkillElement";
+import skillEntries from "/src/data/skillEntries.json";
+
+// 5-proficent, 4-competent , 3-experienced, 2-beginner, 1-novice
+
+function getSortedEntries() {
+  let sortedEntries = {};
+
+  for (const entry of skillEntries) {
+    const type = entry.type;
+    if (type in sortedEntries === false) {
+      sortedEntries[type] = [];
+    }
+    sortedEntries[type].push(entry);
+  }
+  return sortedEntries;
+}
+
+function getSkillList(entries, skillType) {
+  return (
+    <Fragment key={"fragment-" + skillType}>
+      <h3 key={"skill-h3-" + skillType} className="skill-list-h3">
+        {skillType}
+      </h3>
+      <div key={"skill-list-" + skillType} className="skill-list-div">
+        {entries.map((entry) => (
+          <SkillElement
+            key={entry.id}
+            title={entry.title}
+            icon={entry.icon}
+            skillLevel={entry.level}
+          />
+        ))}
+      </div>
+    </Fragment>
+  );
+}
+
 function Work() {
+  const sortedEntries = getSortedEntries();
   return (
     <main>
       <h1>Software Development</h1>
       <section className="main-list-section">
-        <h3>Languages</h3>
-        <div className="skill-list-div">
-        <SkillElement 
-          title="Python"
-        />
-        <SkillElement 
-          title="PHP"
-        />
-        <SkillElement 
-          title="Intersystem ObjectScript"
-        />
-        </div>
-      </section>
-      <section className="main-list-section">
-        <h3>Frameworks & Libraries</h3>
-        <div className="skill-list-div">
-        <SkillElement 
-          title="FastAPI"
-        />
-        <SkillElement 
-          title="Apache Airflow"
-        />
-        </div>
+        {Object.keys(sortedEntries).map((type) =>
+          getSkillList(sortedEntries[type], type)
+        )}
       </section>
     </main>
   );
