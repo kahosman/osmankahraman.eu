8b2e2190f26be7d9e545a6057cbd98644c61c5b7
Osman Kahraman <iam@osmankahraman.eu>
2025-07-03 16:47:55 +0200

    Added animated title bar globe icon, a view transition when changing the page to mock "loading", a tab icon for each page with an animated loading tab icon during view transition, Changed various styling for the title bar and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -10,10 +10,51 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const location = useLocation();
-  console.log(location.pathname);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    let classIconString;
+    const tabLoadingIconEl = document.getElementById("tabLoadingIcon");
+
+    switch (location.pathname) {
+      case "/":
+        classIconString = "bxs-home-heart";
+        break;
+      case "/bio":
+        classIconString = "bxs-user-square";
+        break;
+      case "/work":
+        classIconString = "bxs-briefcase-alt-2";
+        break;
+      case "/projects":
+        classIconString = "bxs-folder-code";
+        break;
+    }
+
+    tabLoadingIconEl.setAttribute("class", "");
+    tabLoadingIconEl.classList.add("bx", "bx-loader-dots");
+
+    const timer = setTimeout(() => {
+      tabLoadingIconEl.setAttribute("class", "");
+      tabLoadingIconEl.classList.add("bx", classIconString);
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [location]);
   return (
     <>
-      <Header currentPage={location.pathname} />
+      <Header currentPage={location.pathname} isLoading={loading} />
+      {loading && (
+        <div className="loading-click-block">
+          <div className="loading-overlay">
+            <div className="loading-background"></div>
+            <div className="loading-spinner"></div>
+          </div>
+        </div>
+      )}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/bio" element={<Biography />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router";
 import "./Header.css"
+import globeIcon from "/src/assets/images/ie4s.webp"
 function Header(props) {
   let currentPage = ""
   if (props.currentPage == "/") {
@@ -11,17 +12,17 @@ function Header(props) {
   return (
     <header>
       <div className="title-bar-div">
-        <i className='bx  bx-globe-alt'  ></i><h2 className="title-bar-h2">{currentPage} - Osman Kahraman</h2>
+        <img className="title-bar-globe-img" src={globeIcon}/><h2 className="title-bar-h2">{currentPage} - Osman Kahraman</h2>
       </div>
       <nav>
         <div className="nav-top-row-div">
-          <NavLink to="/" end><button className="nav-button">Home</button></NavLink>
-          <NavLink to="/bio" end><button className="nav-button">Bio</button></NavLink>
-          <NavLink to="/work" end><button className="nav-button">Work</button></NavLink>
-          <NavLink to="/projects" end><button className="nav-button">Projects</button></NavLink>
+          <NavLink to="/" end viewTransition><button className="nav-button">Home</button></NavLink>
+          <NavLink to="/bio" end viewTransition><button className="nav-button">Bio</button></NavLink>
+          <NavLink to="/work" end viewTransition><button className="nav-button">Work</button></NavLink>
+          <NavLink to="/projects" end viewTransition><button className="nav-button">Projects</button></NavLink>
         </div>
         <div className="nav-bottom-row-div">
-          <span className="current-tab-span">{currentPage}</span>
+          <div className="current-tab-div"><i id="tabLoadingIcon" className="bx"/><p>{currentPage}</p></div>
           <input className="nav-search-bar-input" type="text" placeholder="Search"></input>
         </div>
         <div className="tab-accent-line-span"></div>
