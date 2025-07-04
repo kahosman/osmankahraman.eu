aa42e69b929e20646c5ca0e0d9592629976d1a94
Osman Kahraman <iam@osmankahraman.eu>
2025-07-04 19:18:42 +0200

    Fixed images not being displayed after build, because static assets need to be resolved during build using a URL

--- src/components/SkillElement.jsx
+++ src/components/SkillElement.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router";
+function getImageUrl(name) {
+  return new URL(`/src/assets/icons/${name}`, import.meta.url).href
+}
 function SkillElement(props) {
   const linkPath = "/work/"+props.name
   return (
     <Link to={linkPath}>
       <div className="skill-item-div">
-        <img className="skill-icon" src={"/src/assets/icons/" + props.icon} />
+        <img className="skill-icon" src={getImageUrl(props.icon)} />
         <p className="skill-title-p">{props.title}</p>
       </div>
     </Link>
